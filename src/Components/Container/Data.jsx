a7e16b00481068a5225800e48cdac7aec3c5e6b6
rviscal <113106260+rviscal@users.noreply.github.com>
2023-02-14 11:52:08 -0300

    identação

--- src/Components/Container/Data.jsx
+++ src/Components/Container/Data.jsx
@@ -1,32 +1,38 @@
 export const Data = [
-    {
-        bcolor: '#FF6363',
-        htex1: 'Título do card',
-        ptex1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.'
-    },
-    {
-        bcolor: '#63ECFF',
-        htex1: 'Título do card',
-        ptex1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.'
-    },
-    {
-        bcolor: '#F363FF',
-        htex1: 'Título do card',
-        ptex1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.'
-    },
-    {
-        bcolor: '#63FF73',
-        htex1: 'Título do card',
-        ptex1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.'
-    },
-    {
-        bcolor: '#FFDD63',
-        htex1: 'Título do card',
-        ptex1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.'
-    },
-    {
-        bcolor: '#6663FF',
-        htex1: 'Título do card',
-        ptex1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.'
-    }
+  {
+    bcolor: "#FF6363",
+    htex1: "Título do card",
+    ptex1:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.",
+  },
+  {
+    bcolor: "#63ECFF",
+    htex1: "Título do card",
+    ptex1:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.",
+  },
+  {
+    bcolor: "#F363FF",
+    htex1: "Título do card",
+    ptex1:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.",
+  },
+  {
+    bcolor: "#63FF73",
+    htex1: "Título do card",
+    ptex1:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.",
+  },
+  {
+    bcolor: "#FFDD63",
+    htex1: "Título do card",
+    ptex1:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.",
+  },
+  {
+    bcolor: "#6663FF",
+    htex1: "Título do card",
+    ptex1:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.",
+  },
 ];
